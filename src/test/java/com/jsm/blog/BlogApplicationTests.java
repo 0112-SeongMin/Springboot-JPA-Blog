da1fa1fd1dd4e84fb6a1dde57946ab90038b7350
DESKTOP-3FHHM6M\gkswk <gkswk89@naver.com>
2022-12-09 12:34:24 +0900

    환경세팅완료 v1

---
+++ src/test/java/com/jsm/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsm.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
